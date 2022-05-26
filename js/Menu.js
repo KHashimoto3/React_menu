c9eb69692d8ac1f05601db4b8e9ce30f98dfeeec
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-05-26 22:29:18 +0900

    React（JSX）の導入

---
+++ js/Menu.js
@@ -0,0 +1,11 @@
+var element = React.createElement(
+  React.Fragment,
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "React\u306E\u5C0E\u5165\u6210\u529F\uFF01"
+  )
+);
+
+ReactDOM.render(element, document.getElementById("menu_area"));
